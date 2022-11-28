8be9063a7457e23e32f5bcc28f2dbbb9e2f2dc79
Rossy Tshitshi <97832482+RossyTshitshi@users.noreply.github.com>
2022-11-28 15:37:12 +0100

    Mis à jour main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,4 @@
-/**
-* Template Name: Scaffold - v2.2.1
-* Template URL: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
+
 !(function($) {
   "use strict";
 
